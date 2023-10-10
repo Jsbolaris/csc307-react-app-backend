914013766a180baf0706913c745579e444129454
jsbol <jsbolaris@gmail.com>
2023-10-10 01:13:55 -0700

    part 1-7 done

--- backend.js
+++ backend.js
@@ -41,8 +41,12 @@ app.get('/', (req, res) => {
 app.get('/users/:id', (req, res) => {
    const id = req.params['id']; //or req.params.id
    let result = findUserById(id);
+   if(result = undefined){
+      const name = req.params['name'];
+      result = findUserByName(name);
+   }
    if (result === undefined || result.length == 0)
-       res.status(404).send('Resource not found.');
+       res.status(404).send('Resource not found.' + result);
    else {
        result = {users_list: result};
        res.send(result);
@@ -53,22 +57,62 @@ function findUserById(id) {
    return users['users_list'].find( (user) => user['id'] === id); // or line below
    //return users['users_list'].filter( (user) => user['id'] === id);
 }
+
+function findIndexById(id) {
+   return users['users_list'].findIndex( (user) => user['id'] === id);
+}
+
 app.get('/users', (req, res) => {
    const name = req.query.name;
-   if (name != undefined){
-       let result = findUserByName(name);
+   const job = req.query.job;
+   if (name != undefined && job != undefined){
+       let result = findUserByNameJob(name, job);
        result = {users_list: result};
        res.send(result);
    }
    else{
-       res.send(users);
+      res.send('name = ' + name + 'job = ' + job)
+      res.send(users);
    }
 });
+
+
 
 const findUserByName = (name) => { 
    return users['users_list'].filter( (user) => user['name'] === name); 
 }
 
+const findUserByNameJob = (name, job) => { 
+   return users['users_list'].filter( ((user) => user['name'] === name) && ((user) => user['job'] === job)); 
+}
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });    
+
+app.post('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.status(200).end();
+});
+
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id'];
+   const index = findIndexById(id);
+   if(index > -1){
+      deleteUser(index);
+      res.status(200).end();
+   } else{
+      res.status(404).send('Invalid index: ' + index);
+   }
+}
+)
+
+function addUser(user){
+   users['users_list'].push(user);
+}
+
+function deleteUser(index){
+   users['users_list'].splice(index, 1);
+}
